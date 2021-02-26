ae4602792c8de1f4b215d311d098aaf086d33542
Hotococoa6078 <71052778+Hotococoa6078@users.noreply.github.com>
2021-02-27 03:52:53 +0900

    sans

--- sans.js
+++ sans.js
@@ -1,3 +1,17 @@
+//---------------------------------------------------------------------------------------------------------------
+//개발자 : Hoto_Cocoa6078
+//해당 코드에 대한 라이선스는 개발자 본인에게 없으며, 이를 무단으로 복제, 타 목적으로 사용할시엔 기타 통지 없이 처벌이 이루어지지 않습니다.
+//해당 문서에 대한 저작권은 개발자에게 있으며, 기타 상용관련 문의는 샌즈에게 전송해주십시오.( 이메일 : wasans@undertale.com )
+//---------------------------------------------------------------------------------------------------------------
+//기타 함수
+//---------------------------------------------------------------------------------------------------------------
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+//---------------------------------------------------------------------------------------------------------------
+//사이트 버튼별 함수
+//---------------------------------------------------------------------------------------------------------------
 function pointUp(){
     var tempEle = document.createElement("div")
     document.getElementById("wa").append(tempEle)
@@ -64,7 +78,7 @@ function banbok(){
     document.getElementById('wa').append(tempEle)
     clearInterval(test)
 }
-var ttest
+//var ttest
 var dd = false
 function stimeout(){
     dd = false
@@ -73,7 +87,7 @@ function stimeout(){
     var tt = prompt("몇초 후에 실행시킬건가요?", 1)
     if(tt){
         dd = true
-        ttest = setTimeout(function() {
+        var ttest = setTimeout(function() {
             tempEle.innerHTML = tt+"초 후에 나온 텍스트"
             dd = false
         }, tt*1000)
@@ -124,12 +138,84 @@ function changec(){
         i++
     }
 }
+function printip(){
+    var temp = document.createElement('div')
+    document.getElementById('wa').append(temp)
+    if(!navigator.onLine){
+        temp.innerHTML = '현재 오프라인상태이므로 IP조회가 불가능합니다.'
+    }else{
+        var ifaces = os.networkInterfaces()
+        var ip = ''
+        for(var dev in ifaces){
+            var alias = 0
+            ifaces[dev].forEach(function(details){
+                if(details.family == 'IPv4' && details.internal === false){
+                    ip = details.address
+                    ++alias
+                }
+            })
+        }
+        temp.innerHTML = '현재 IP : ' + ip
+    }
+}
+function cheakonline(){
+    var temp = document.createElement('div')
+    document.getElementById('wa').append(temp)
+    temp.innerHTML = "온라인 여부 : " + navigator.onLine
+}
+function test(){
+    var temp = document.createElement('div')
+    document.getElementById('wa').append(temp)
+    temp.innerHTML = '첫번째 구문'
+    var  timeout = setTimeout(function() {temp.innerHTML = '두번째 구문'}, 2*1000)
+}
+function back(){
+    var temp = document.createElement('div')
+    document.getElementById('wa').append(temp)
+    temp.innerHTML = '재귀함수 만세'
+    back()
+}
+var count = 0
+function changetxt(){
+    var countstr = `text` + count
+    count++
+    var temp = document.createElement(`div`)
+    document.getElementById('wa').append(temp)
+    var text1 = prompt('입력될 텍스트를 입력해주세요.','1번 텍스트')
+    var text2 = prompt('변경될 텍스트를 입력해주세요.','2번 텍스트')
+    var time = prompt('몇초 후에 변경할건가요?','4')
+    temp.innerHTML = text1
+    $('div').addClass(countstr)
+    var timeout = setTimeout(function(){$(`div.${countstr}:last`).text(text2)}, time*1000)
+}
+
+
+
+//마우스 이벤트
+//키보드 이벤트
+$(document).keydown(function(key){
+    $('p.keyd').text('(keydown)누르고 있는 키 : ' + key.keyCode)
+})
+$(document).keypress(function(key){
+    $(`p.keyp`).text('(keypress)누른 키 : ' + key.keyCode)
+})
+//로딩 이벤트
+$(document).load(function(){
+    try{
+        var temp = document.createElement('div')
+        document.getElementById('wa').append(temp)
+        temp.innerHTML = "모든 요소 로딩 완료"
+    }catch(e){
+        temp.innerHTML = '로딩에 실패한 요소가 있습니다. \n에러 내용 : '+ e
+    }
+})
 
 $(".onMouse").on("mouseover focus", function() {
     var tempEle = document.createElement('div')
     document.getElementById('wa').append(tempEle)
-    tempEle.innerHTML = "샌즈"
+    tempEle.innerHTML = "onMouse*"
 })
+
 //여기서부터ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 /*
 $(".printip").ajax({
